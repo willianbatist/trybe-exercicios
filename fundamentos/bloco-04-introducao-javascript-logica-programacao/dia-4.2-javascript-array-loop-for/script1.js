c46825182327b72506fa6d880a2c870181937747
Willian <willian.alves.b15@gmail.com>
2021-10-13 21:13:41 -0300

    Adicionando todas as atividades de fixaçao de conteudo

---
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-4.2-javascript-array-loop-for/script1.js
@@ -0,0 +1,9 @@
+/*
+Exercício 1
+Obtenha o valor "Serviços" do array menu :
+*/
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.indexOf('Serviços');
+
+console.log(menuServices);
