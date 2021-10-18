0737435b54aba87e6bf6b47c22a0cfecfad752e7
Willian <willian.alves.b15@gmail.com>
2021-10-18 14:19:18 -0300

    Alteraçoes

--- fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-4.4-javascript-objetos-funções/exercicio_f.2.js
+++ fundamentos/bloco-04-introducao-javascript-logica-programacao/dia-4.4-javascript-objetos-funções/exercicio_f.2.js
@@ -1,3 +1,19 @@
-let myName = 'Willian';
-let stopMyName = myName.split('').reverse().join('');
-console.log(stopMyName);
+let numbers = [2, 3, 6, 7, 10, 1];
+let maior;
+let index;
+let resultado;
+function bigValue (numbers) {
+    maior = Math.max.apply(null, numbers );
+    for (let index of numbers);
+    if (index = maior) {
+        resultado = index === maior;
+    }
+    return resultado;
+    //for (index = 0; index < numbers.length; index += 1); {
+        // console.log(index)
+    //}
+    
+    //console.log(maior === index[numbers])
+}
+bigValue(numbers)
+
