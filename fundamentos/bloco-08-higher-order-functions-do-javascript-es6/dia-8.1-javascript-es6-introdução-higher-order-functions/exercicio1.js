d8ffcdc9632da0b8fae556cea24ab4f9005521bb
Willian <willian.alves.b15@gmail.com>
2021-11-17 21:31:35 -0300

    exercicio 1

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.1-javascript-es6-introdução-higher-order-functions/exercicio1.js
@@ -0,0 +1,15 @@
+const newEmployees = (callback) => {
+    const employees = {
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+const gerarEmail = (nomeCompleto) => {
+    let emailDaPessoas = `${nomeCompleto.replace(' ', '_').upp}@trybe.br`;
+    return { name: nomeCompleto, email: emailDaPessoas};
+};
+console.log(newEmployees(gerarEmail));
+
