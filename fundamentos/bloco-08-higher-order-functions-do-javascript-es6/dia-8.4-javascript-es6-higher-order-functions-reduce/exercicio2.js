314718f56e416023f9da52aa4c7d71b5a9558660
Willian <willian.alves.b15@gmail.com>
2021-11-22 22:21:30 -0300

    exercicio 3

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.4-javascript-es6-higher-order-functions-reduce/exercicio2.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-8.4-javascript-es6-higher-order-functions-reduce/exercicio2.js
@@ -1,5 +1,5 @@
 const books = require('./books');
-
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
 const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
 
 function reduceNames() {
