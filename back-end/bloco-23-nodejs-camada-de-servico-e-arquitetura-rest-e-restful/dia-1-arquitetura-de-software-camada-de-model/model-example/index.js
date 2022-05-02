27a89b5e8205bc50fd4f374f85183e73722a93bb
Willian <willian.alves.b15@gmail.com>
2022-05-02 15:44:00 -0300

    creat function show authors/id

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -10,6 +10,16 @@
         res.status(200).json(authors);
     });
 
+    app.get('/authors/:id', async (req, res) => {
+        const { id } = req.params;
+
+        const author = await Author.findById(id);
+
+        if (!author) return res.status(404).json({ message: 'Not found' });
+
+        res.status(200).json(author);
+    });
+
     const PORT = process.env.PORT || 3002;
 
     app.listen(PORT, () => {
