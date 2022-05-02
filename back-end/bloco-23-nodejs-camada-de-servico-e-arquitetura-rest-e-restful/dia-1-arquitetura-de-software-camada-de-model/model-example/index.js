d8b5132f4f289f9204291c6e90f4190cd7491594
Willian <willian.alves.b15@gmail.com>
2022-05-02 15:30:08 -0300

    feat: creat get /authores show names authors

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,8 +1,14 @@
     // index.js
 
     const express = require('express');
-
+    const Author = require('./models/Author')
     const app = express();
+
+    app.get('/authors', async (_req, res) => {
+        const authors = await Author.getAll();
+
+        res.status(200).json(authors);
+    });
 
     const PORT = process.env.PORT || 3002;
 
