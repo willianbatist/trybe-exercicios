dcd84ed9c43732b520f830a1d457900e9838c5c0
Willian <willian.alves.b15@gmail.com>
2022-05-02 15:58:10 -0300

    end fixando, create method post

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,8 +1,10 @@
     // index.js
 
     const express = require('express');
+    const bodyParser = require('body-parser');
     const Author = require('./models/Author')
     const app = express();
+    app.use(bodyParser.json());
 
     app.get('/authors', async (_req, res) => {
         const authors = await Author.getAll();
@@ -18,6 +20,18 @@
         if (!author) return res.status(404).json({ message: 'Not found' });
 
         res.status(200).json(author);
+    });
+
+    app.post('/authors', async (req, res) => {
+        const { first_name, middle_name, last_name } = req.body;
+
+        if (!Author.isValid(first_name, middle_name, last_name)) {
+            return res.status(400).json({ message: 'Dados inválidos' });
+        }
+
+        await Author.create(first_name, middle_name, last_name);
+
+        res.status(201).json({ message: 'Autor criado com sucesso! '});
     });
 
     const PORT = process.env.PORT || 3002;
