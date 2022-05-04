9aa6e6718507c6df45beec0cddfaf1dbf2851651
Willian <willian.alves.b15@gmail.com>
2022-05-03 21:32:55 -0300

    create CepModel.js

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/cep-lookup/index.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (_req, res) => res.status(200).json({ message: 'funcionando'}) );
+
 app.get('/ping', (_req, res) => res.status(200).json({ message: "pong!" }) );
+
+app.get('/cep/:cep', (req, res) => {
+  
+});
 
 const PORT = 3000;
 
