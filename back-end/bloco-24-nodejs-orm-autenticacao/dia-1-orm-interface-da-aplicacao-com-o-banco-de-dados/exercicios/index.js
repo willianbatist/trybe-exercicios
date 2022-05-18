cd3a85df19132a3c2406f60a3fb7221ab848f228
Willian <willian.alves.b15@gmail.com>
2022-05-18 11:31:42 -0300

    feat: finish day 24.1

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicios/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const { books } = require('./models');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/books', (_req, res) => res.status(200).json({ name: "Willian" }))
+app.get('/books', async (_req, res) => {
+    try {
+      const booksFind = await books.findAll();
+      console.log(booksFind);
+      return res.status(200).json(booksFind)
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+})
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
